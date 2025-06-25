49ac5f4b3700c7298eabb59defc8fe48e9764e95
Priyan Rai <54280820+inmert@users.noreply.github.com>
2025-06-25 11:25:06 +0530

    Initial Commit

---
+++ Manta/MantaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MantaApp.swift
+//  Manta
+//
+//  Created by Priyan Rai on 25/06/25.
+//
+
+import SwiftUI
+
+@main
+struct MantaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
